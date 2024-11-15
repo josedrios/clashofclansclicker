3c0844c5214aa6b0b580403cd42390ce8aefe9c5
josedrios <joserios0531@gmail.com>
2024-11-15 15:36:52 -0800

    Made cv imshows for each currency, for the most part the number extraction from the frames (ROI's to be specific) are rather consistent, still a noticable margin of error but it can be overlooked

--- iphone.py
+++ iphone.py
@@ -3,12 +3,6 @@ import numpy as np
 import mss
 import pytesseract
 
-#x, y, w, h = 275, 200, 230, 160
-
-x, y, w, h = 275, 200, 230, 160
-
-
-
 # Initialize mss for screen capture
 with mss.mss() as sct:
     monitor = sct.monitors[1]  # Adjust for the desired monitor (1 is usually primary)
@@ -16,16 +10,41 @@ with mss.mss() as sct:
     while True:
         screen = sct.grab(monitor)
         frame = np.array(screen)
+        config = ('-l eng --oem 1 --psm 6')
         
-        roi = frame[y:y+h, x:x+w]
+        # GOLD
+        x, y, w, h = 275, 200, 160, 40
+        roi = frame[y:y+h, x:x+w] 
+        roi = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
+        cv2.imshow("GOLD", roi)
 
-        gray_frame = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY) 
+        text = pytesseract.image_to_string(roi, config=config)
+        text = text.rstrip('\n')
 
-        text = pytesseract.image_to_string(gray_frame)
+        print(F"GOLD: {text}")
 
-        print(text)
+        # ELIXIR
+        y_elixir = y + h + 15
+        roi = frame[y_elixir:y_elixir+h, x:x+w]
+        roi = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
+        cv2.imshow("ELIXIR", roi)
 
-        cv2.imshow("Screen Capture", gray_frame)
+        text = pytesseract.image_to_string(roi, config=config)
+        text = text.rstrip('\n')
+
+        print(F"ELIXIR: {text}")
+
+        # DARK
+        y_dark = y_elixir + h + 20
+        w_dark = w - 40
+        roi = frame[y_dark:y_dark+h, x:x+w_dark]
+        roi = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
+        cv2.imshow("DARK", roi)
+
+        text = pytesseract.image_to_string(roi, config=config)
+        text = text.rstrip('\n')
+
+        print(F"DARK: {text}")
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
