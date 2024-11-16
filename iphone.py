0620ed3ebfff9c6f86bad8a9cb0334f85acb6769
josedrios <joserios0531@gmail.com>
2024-11-15 18:55:40 -0800

    Making adjustments to the frame preprocessor (still needs to be tweaked, was improved though)

--- iphone.py
+++ iphone.py
@@ -20,8 +20,23 @@ def string_extraction_and_cleanup(roi, config, currency):
     else:
         resources[currency] = ""
         resources[currency] = cleaned_string
-        print(f"GOLD: {resources['GOLD']} | ELIXIR: {resources['ELIXIR']} | DARK: {resources['DARK']}", end='\r')
+        print(f"GOLD: {resources['GOLD']} | ELIXIR: {resources['ELIXIR']} | DARK: {resources['DARK']}"+" "*10, end='\r')
         is_empty = 0
+
+def image_processing(currency, frame, frame_config, window_coords):
+    x, y, w, h = frame_config
+    roi = frame[y:y+h, x:x+w]
+    roi = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
+    roi = cv2.medianBlur(roi, 3)
+    _, roi = cv2.threshold(roi, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    roi = cv2.resize(roi, None, fx=2, fy=2, interpolation=cv2.INTER_CUBIC)
+    kernel = np.ones((2,2), np.uint8)
+    roi = cv2.dilate(roi, kernel, iterations=1)
+    roi = cv2.erode(roi, kernel, iterations=1)
+    roi = cv2.morphologyEx(roi, cv2.MORPH_OPEN, kernel)
+    cv2.imshow(currency, roi)
+    cv2.moveWindow(currency, window_coords[0], window_coords[1])
+    return roi
 
 # Initialize mss for screen capture
 with mss.mss() as sct:
@@ -31,35 +46,25 @@ with mss.mss() as sct:
         screen = sct.grab(monitor)
         frame = np.array(screen)
         config = ('--psm 6 --oem 3 -c tessedit_char_whitelist=0123456789')
-        x, y, w, h = 275, 200, 170, 40
+        # Base Frame Coordinates/Layout
         
         # GOLD
-        roi = frame[y:y+h, x:x+w] 
-        roi = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
-        cv2.imshow("GOLD", roi)
-        cv2.moveWindow("GOLD", 0, 120)
-
+        frame_config = [275, 200, 170, 40]
+        window_coords = [0, 120]
+        roi = image_processing("GOLD", frame, frame_config, window_coords)
         string_extraction_and_cleanup(roi, config, "GOLD")
 
         # ELIXIR
-        y_elixir = y + h + 20
-        roi = frame[y_elixir:y_elixir+h, x:x+w]
-        roi = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
-        cv2.imshow("ELIXIR", roi)
-        cv2.moveWindow("ELIXIR", 200, 120)
-
+        frame_config = [275, 260, 170, 40]
+        window_coords = [200, 120]
+        roi = image_processing("ELIXIR", frame, frame_config, window_coords)
         string_extraction_and_cleanup(roi, config, "ELIXIR")
 
         # DARK
-        y_dark = y_elixir + h + 20
-        w_dark = w - 40
-        roi = frame[y_dark:y_dark+h, x:x+w_dark]
-        roi = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
-        cv2.imshow("DARK", roi)
-        cv2.moveWindow("DARK", 400, 120)
-
+        frame_config = [275, 320, 130, 40]
+        window_coords = [400, 120]
+        roi = image_processing("DARK", frame, frame_config, window_coords)
         string_extraction_and_cleanup(roi, config, "DARK")
-
 
         if is_empty > 4 and not new_base_message_shown:
             print("NEW BASE DETECTED"+" "*40, end='\r')
